771cf02ca41e9b217f2611d99a4f856fdafd4a98
Nikhil Raj <nrjvoo33@gmail.com>
2022-12-10 17:34:26 +0530

    Sri Ganesay Namah
    Signed-off-by: Nikhil Raj <nrjvoo33@gmail.com>

---
+++ ozod/src/main/java/com/org/ozod/OzodApplication.java
@@ -0,0 +1,13 @@
+package com.org.ozod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OzodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OzodApplication.class, args);
+	}
+
+}
---
+++ ozod/src/test/java/com/org/ozod/OzodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.ozod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OzodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
